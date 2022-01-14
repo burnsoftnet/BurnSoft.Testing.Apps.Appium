8f7c6c4faa14a78a6b986f274c68c50b495d6948
Joe M <bs.github@burnsoft.net>
2022-01-14 08:36:56 -0500

    #1 started general class

---
+++ BurnSoft.Testing.Apps.Appium/GeneralActions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BurnSoft.Testing.Apps.Appium
+{
+    /// <summary>
+    /// The General Actions Class is the main class that will found to common functions used to communicate with selenium
+    /// </summary>
+    public class GeneralActions
+    {
+    }
+}
