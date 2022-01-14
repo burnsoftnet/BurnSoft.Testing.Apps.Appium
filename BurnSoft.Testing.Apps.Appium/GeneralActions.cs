a93436e6dbb971ab0221341b103e4f3d1038e113
Joe M <bs.github@burnsoft.net>
2022-01-14 09:57:52 -0500

    #1 adding vaiables

--- BurnSoft.Testing.Apps.Appium/GeneralActions.cs
+++ BurnSoft.Testing.Apps.Appium/GeneralActions.cs
@@ -13,5 +13,49 @@ namespace BurnSoft.Testing.Apps.Appium
     /// </summary>
     public class GeneralActions
     {
+        #region "Private Variables"
+        private string _windowsApplicationDriverUrl;
+        private string _WinAppDriverPath;
+        #endregion
+        #region "Public Variables"
+        /// <summary>
+        /// Gets or sets the windows application driver URL. If not set, it will default to http://127.0.0.1:4723
+        /// </summary>
+        /// <value>The windows application driver URL.</value>
+        public string WindowsApplicationDriverUrl
+        {
+            get
+            {
+                if (_windowsApplicationDriverUrl == null) return "http://127.0.0.1:4723";
+                if (_windowsApplicationDriverUrl.Length == 0)
+                {
+                    return "http://127.0.0.1:4723";
+                }
+                else
+                {
+                    return _windowsApplicationDriverUrl;
+                }
+            }
+            set => _windowsApplicationDriverUrl = value;
+        }
+
+        public string WinAppDriverPath
+        {
+            get {
+                if (_WinAppDriverPath == null) return @"C:\Program Files (x86)\Windows Application Driver\WinAppDriver.exe";
+                if (_WinAppDriverPath.Length == 0)
+                {
+                    return @"C:\Program Files (x86)\Windows Application Driver\WinAppDriver.exe";
+                }
+                else
+                {
+                    return _WinAppDriverPath;
+                }
+            }
+            set => _WinAppDriverPath = value;
+        }
+
+        #endregion
+
     }
 }
