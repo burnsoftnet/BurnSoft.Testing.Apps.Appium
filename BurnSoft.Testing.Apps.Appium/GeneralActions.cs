de8e3fb3d3e98e8bdd328778f5b17c727acd5ab2
Joe M <bs.github@burnsoft.net>
2022-01-14 08:38:16 -0500

    #2 Added Comment

--- BurnSoft.Testing.Apps.Appium/GeneralActions.cs
+++ BurnSoft.Testing.Apps.Appium/GeneralActions.cs
@@ -7,7 +7,9 @@ using System.Threading.Tasks;
 namespace BurnSoft.Testing.Apps.Appium
 {
     /// <summary>
-    /// The General Actions Class is the main class that will found to common functions used to communicate with selenium
+    /// The General Actions Class is the main class that will found to common functions used to communicate with appium.  Just like I did
+    /// in the selenium helper library, this will be the main class in case there are other special classes that need to be created to have it work with
+    /// other OS's or application types, etc.
     /// </summary>
     public class GeneralActions
     {
