f155596dfb891fb5d65ce6b24ce994726b9ab442
Joe M <bs.github@burnsoft.net>
2022-01-17 09:20:03 -0500

    #3 created unit test to test out new function

---
+++ BurnSoft.Testing.Apps.Appium.UnitTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("BurnSoft.Testing.Apps.Appium.UnitTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("BurnSoft.Testing.Apps.Appium.UnitTest")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("025d2f7c-d91c-49d4-9bea-979a449cd1b2")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
