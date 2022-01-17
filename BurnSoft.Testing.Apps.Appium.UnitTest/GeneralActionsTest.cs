1f85b45cf7c3eca5c4839d30dd26332c656c5338
Joe M <bs.github@burnsoft.net>
2022-01-17 10:36:54 -0500

    #3 added init

--- BurnSoft.Testing.Apps.Appium.UnitTest/GeneralActionsTest.cs
+++ BurnSoft.Testing.Apps.Appium.UnitTest/GeneralActionsTest.cs
@@ -6,9 +6,20 @@ namespace BurnSoft.Testing.Apps.Appium.UnitTest
     [TestClass]
     public class GeneralActionsTest
     {
+        public TestContext TestContext { get; set; }
+        private GeneralActions _ga;
+        [TestInitialize]
+        public void Init()
+        {
+            _ga = new GeneralActions();
+            _ga.ApplicationPath = "F:\\Source\\Repos\\MyGunCollection\\BSMyGunCollection\\bin\\Debug\\BSMyGunCollection.exe";
+            _ga.Inititalize();
+        }
+
         [TestMethod]
         public void TestMethod1()
         {
+
         }
     }
 }
