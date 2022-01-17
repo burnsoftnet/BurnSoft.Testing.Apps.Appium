92dcb53f03833ee178c98bd2bbb2517f467bfa50
Joe M <bs.github@burnsoft.net>
2022-01-17 09:13:43 -0500

    #1 updated projected settings

--- BurnSoft.Testing.Apps.Appium/Properties/AssemblyInfo.cs
+++ BurnSoft.Testing.Apps.Appium/Properties/AssemblyInfo.cs
@@ -6,12 +6,12 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("BurnSoft.Testing.Apps.Appium")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Appium library helper, This library was deisgned to use appium for app Ui testing quick and easy.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("www.burnsoft.net")]
 [assembly: AssemblyProduct("BurnSoft.Testing.Apps.Appium")]
-[assembly: AssemblyCopyright("Copyright ©  2022")]
-[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCopyright("Copyright ©www.burnsoft.net  2022")]
+[assembly: AssemblyTrademark("www.burnsoft.net")]
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible
