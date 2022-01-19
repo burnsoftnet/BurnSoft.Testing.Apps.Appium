cc4375b6e62f27236f282d25f876af59ee888542
burnsoftnet <bs.github@burnsoft.net>
2022-01-19 09:21:01 -0500

    #3 deleted tests that was obsolete

--- BurnSoft.Testing.Apps.Appium.UnitTest/GeneralActionsTest.cs
+++ BurnSoft.Testing.Apps.Appium.UnitTest/GeneralActionsTest.cs
@@ -34,36 +34,7 @@ namespace BurnSoft.Testing.Apps.Appium.UnitTest
             _ga.Dispose();
         }
 
-        [TestMethod]
-        public void ClickOnFirstObjectInList()
-        {
-            bool value = false;
-            try
-            {
-                value = _ga.ClickOnElement(_automationId, out _errOut, GeneralActions.AppAction.FindElementByName);
-                if (_errOut.Length > 0) throw new Exception(_errOut);
-            }
-            catch (Exception e)
-            {
-                TestContext.WriteLine($"ERROR: {e.Message}");
-            }
-            Assert.IsTrue(value);
-        }
-        [TestMethod]
-        public void DoubleCLickElementTest()
-        {
-            bool value = false;
-            try
-            {
-                value = _ga.DoubleCLickElement(_automationId, out _errOut, GeneralActions.AppAction.FindElementByName);
-                if (_errOut.Length > 0) throw new Exception(_errOut);
-            }
-            catch (Exception e)
-            {
-                TestContext.WriteLine($"ERROR: {e.Message}");
-            }
-            Assert.IsTrue(value);
-        }
+        
 
         [TestMethod]
         public void PerformActionDoubleCLickElementTest()
