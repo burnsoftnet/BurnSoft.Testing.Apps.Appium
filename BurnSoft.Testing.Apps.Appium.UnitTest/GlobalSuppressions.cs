15f95554fa0f34e937f21c2beb68b23fd9b478d6
BurnSoft <bs.github@burnsoft.net>
2022-03-16 09:09:46 -0400

    #5 added to supprssion

---
+++ BurnSoft.Testing.Apps.Appium.UnitTest/GlobalSuppressions.cs
@@ -0,0 +1,9 @@
+﻿// This file is used by Code Analysis to maintain SuppressMessage
+// attributes that are applied to this project.
+// Project-level suppressions either have no target or are given
+// a specific target and scoped to a namespace, type, member, etc.
+
+using System.Diagnostics.CodeAnalysis;
+
+[assembly: SuppressMessage("CodeQuality", "IDE0052:Remove unread private members", Justification = "<Pending>", Scope = "member", Target = "~F:BurnSoft.Testing.Apps.Appium.UnitTest.ScratchPad._automationId")]
+[assembly: SuppressMessage("Style", "IDE0017:Simplify object initialization", Justification = "<Pending>", Scope = "member", Target = "~M:BurnSoft.Testing.Apps.Appium.UnitTest.ScratchPad.Init")]
