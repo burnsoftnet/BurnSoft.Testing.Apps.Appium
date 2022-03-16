0d0a28ac4d9c8a7dc9ac51f5e65eaec6d03ec1b0
BurnSoft <bs.github@burnsoft.net>
2022-03-16 09:06:50 -0400

    #4 updated json library

---
+++ BurnSoft.Testing.Apps.Appium/GlobalSuppressions.cs
@@ -0,0 +1,14 @@
+﻿// This file is used by Code Analysis to maintain SuppressMessage
+// attributes that are applied to this project.
+// Project-level suppressions either have no target or are given
+// a specific target and scoped to a namespace, type, member, etc.
+
+using System.Diagnostics.CodeAnalysis;
+
+[assembly: SuppressMessage("Style", "IDE0044:Add readonly modifier", Justification = "<Pending>", Scope = "member", Target = "~F:BurnSoft.Testing.Apps.Appium.GeneralActions._classLocation")]
+[assembly: SuppressMessage("CodeQuality", "IDE0051:Remove unused private members", Justification = "<Pending>", Scope = "member", Target = "~M:BurnSoft.Testing.Apps.Appium.GeneralActions.ErrorMessage(System.String,System.AccessViolationException)~System.String")]
+[assembly: SuppressMessage("CodeQuality", "IDE0051:Remove unused private members", Justification = "<Pending>", Scope = "member", Target = "~M:BurnSoft.Testing.Apps.Appium.GeneralActions.ErrorMessage(System.String,System.ArgumentException)~System.String")]
+[assembly: SuppressMessage("CodeQuality", "IDE0051:Remove unused private members", Justification = "<Pending>", Scope = "member", Target = "~M:BurnSoft.Testing.Apps.Appium.GeneralActions.ErrorMessage(System.String,System.ArgumentNullException)~System.String")]
+[assembly: SuppressMessage("CodeQuality", "IDE0051:Remove unused private members", Justification = "<Pending>", Scope = "member", Target = "~M:BurnSoft.Testing.Apps.Appium.GeneralActions.ErrorMessage(System.String,System.InvalidCastException)~System.String")]
+[assembly: SuppressMessage("Style", "IDE0060:Remove unused parameter", Justification = "<Pending>", Scope = "member", Target = "~M:BurnSoft.Testing.Apps.Appium.GeneralActions.GetElements(System.String,System.String@,BurnSoft.Testing.Apps.Appium.GeneralActions.AppAction)")]
+[assembly: SuppressMessage("Style", "IDE0017:Simplify object initialization", Justification = "<Pending>", Scope = "member", Target = "~M:BurnSoft.Testing.Apps.Appium.GeneralActions.StartWinAppDriver")]
