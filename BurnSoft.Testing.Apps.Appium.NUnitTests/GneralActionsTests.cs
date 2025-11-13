415092bfe675e0e3fe979c9f895780258337a6e1
BurnSoft <bs.github@burnsoft.net>
2025-11-13 16:53:09 -0500

    #38 moved tests over

--- BurnSoft.Testing.Apps.Appium.NUnitTests/GneralActionsTests.cs
+++ BurnSoft.Testing.Apps.Appium.NUnitTests/GneralActionsTests.cs
@@ -73,5 +73,231 @@ namespace BurnSoft.Testing.Apps.Appium.NUnitTests
             }
             //Assert.IsTrue(value);
         }
+
+        /// <summary>
+        /// Defines the test method PerformActionReadElementTest.
+        /// </summary>
+        /// <exception cref="System.Exception"></exception>
+        [Test]
+        public void PerformActionReadElementTest()
+        {
+            bool value = false;
+            try
+            {
+                bool myValue = _ga.PerformAction(_automationId, "", GeneralActions.MyAction.DoubleClick, out _errOut, GeneralActions.AppAction.FindElementByName);
+                if (_errOut.Length > 0) throw new Exception(_errOut);
+                Thread.Sleep(500);
+                string serial = _ga.PerformAction("txtSerial", out _errOut);
+                TestContext.WriteLine($"Serial Number: {serial}");
+                value = serial.Length > 0;
+            }
+            catch (Exception e)
+            {
+                TestContext.WriteLine($"ERROR: {e.Message}");
+            }
+            //Assert.IsTrue(value);
+        }
+        /// <summary>
+        /// Defines the test method PerformActionCLickElementTest.
+        /// </summary>
+        /// <exception cref="System.Exception"></exception>
+        [Test]
+        public void PerformActionCLickElementTest()
+        {
+            bool value = false;
+            try
+            {
+                value = _ga.PerformAction(_automationId, "", GeneralActions.MyAction.Click, out _errOut, GeneralActions.AppAction.FindElementByName);
+                if (_errOut.Length > 0) throw new Exception(_errOut);
+            }
+            catch (Exception e)
+            {
+                TestContext.WriteLine($"ERROR: {e.Message}");
+            }
+            //Assert.IsTrue(value);
+        }
+        /// <summary>
+        /// Defines the test method PerformActionVerifyElementTest.
+        /// </summary>
+        /// <exception cref="System.Exception"></exception>
+        [Test]
+        public void PerformActionVerifyElementTest()
+        {
+            bool value = false;
+            try
+            {
+                value = _ga.PerformAction(_automationId, "", GeneralActions.MyAction.Nothing, out _errOut, GeneralActions.AppAction.FindElementByName);
+                if (_errOut.Length > 0) throw new Exception(_errOut);
+            }
+            catch (Exception e)
+            {
+                TestContext.WriteLine($"ERROR: {e.Message}");
+            }
+            //Assert.IsTrue(value);
+        }
+        /// <summary>
+        /// Defines the test method PerformActionSendTextElementTest.
+        /// </summary>
+        /// <exception cref="System.Exception"></exception>
+        /// <exception cref="System.Exception"></exception>
+        /// <exception cref="System.Exception"></exception>
+        /// <exception cref="System.Exception"></exception>
+        [Test]
+        public void PerformActionSendTextElementTest()
+        {
+            bool value = false;
+            try
+            {
+                if (!_ga.PerformAction("Search Gun Collection", "", GeneralActions.MyAction.Click, out _errOut,
+                    GeneralActions.AppAction.FindElementByName)) throw new Exception(_errOut);
+                Thread.Sleep(1000);
+                if (!_ga.PerformAction("txtLookFor", "", GeneralActions.MyAction.Click, out _errOut)) throw new Exception(_errOut);
+
+                value = _ga.PerformAction("txtLookFor", "Glock", GeneralActions.MyAction.SendKeys, out _errOut);
+                if (_errOut.Length > 0) throw new Exception(_errOut);
+                Thread.Sleep(1000);
+                if (!_ga.PerformAction("btnSearch", "", GeneralActions.MyAction.Click, out _errOut)) throw new Exception(_errOut);
+                Thread.Sleep(500);
+            }
+            catch (Exception e)
+            {
+                TestContext.WriteLine($"ERROR: {e.Message}");
+            }
+
+            foreach (string s in _ga.ScreenShotLocation)
+            {
+                TestContext.WriteLine($"{s}");
+            }
+            foreach (string s in _ga.ErrorLists)
+            {
+                TestContext.WriteLine($"{s}");
+            }
+            //Assert.IsTrue(value);
+        }
+        /// <summary>
+        /// Gets the commands.
+        /// </summary>
+        /// <returns>List&lt;BatchCommandList&gt;.</returns>
+        private List<BatchCommandList> GetCommands()
+        {
+            List<BatchCommandList> cmd = new List<BatchCommandList>();
+            cmd.Add(new BatchCommandList()
+            {
+                TestName = "Search Gun Collection Button",
+                Actions = GeneralActions.MyAction.Click,
+                CommandAction = GeneralActions.AppAction.FindElementByName,
+                ElementName = "Search Gun Collection"
+            });
+            cmd.Add(new BatchCommandList()
+            {
+                TestName = "Verify For Textbox exists",
+                Actions = GeneralActions.MyAction.Nothing,
+                CommandAction = GeneralActions.AppAction.FindElementByAccessibilityId,
+                ElementName = "txtLookFor"
+            });
+            cmd.Add(new BatchCommandList()
+            {
+                TestName = "Look For Textbox",
+                Actions = GeneralActions.MyAction.Click,
+                CommandAction = GeneralActions.AppAction.FindElementByAccessibilityId,
+                ElementName = "txtLookFor"
+            });
+            cmd.Add(new BatchCommandList()
+            {
+                TestName = "Search for word Glock",
+                Actions = GeneralActions.MyAction.SendKeys,
+                CommandAction = GeneralActions.AppAction.FindElementByAccessibilityId,
+                ElementName = "txtLookFor",
+                SendKeys = "Glock"
+            });
+            cmd.Add(new BatchCommandList()
+            {
+                TestName = "Verify Control Combo box Look in",
+                Actions = GeneralActions.MyAction.Nothing,
+                CommandAction = GeneralActions.AppAction.FindElementByAccessibilityId,
+                ElementName = "cmbLookIn"
+            });
+            cmd.Add(new BatchCommandList()
+            {
+                TestName = "Get Control Combo Value box Look in",
+                Actions = GeneralActions.MyAction.ReadValue,
+                CommandAction = GeneralActions.AppAction.FindElementByAccessibilityId,
+                ElementName = "cmbLookIn"
+            });
+            //cmd.Add(new BatchCommandList()
+            //{
+            //    TestName = "Click on Control Combo box Look in",
+            //    Actions = GeneralActions.MyAction.Click,
+            //    CommandAction = GeneralActions.AppAction.FindElementByAccessibilityId,
+            //    ElementName = "cmbLookIn"
+            //});
+            //cmd.Add(new BatchCommandList()
+            //{
+            //    TestName = "Click on Control Combo box Look in",
+            //    Actions = GeneralActions.MyAction.Click,
+            //    CommandAction = GeneralActions.AppAction.FindElementByAccessibilityId,
+            //    ElementName = "Model Name"
+            //});
+            cmd.Add(new BatchCommandList()
+            {
+                TestName = "Start Search",
+                Actions = GeneralActions.MyAction.Click,
+                CommandAction = GeneralActions.AppAction.FindElementByAccessibilityId,
+                ElementName = "btnSearch"
+            });
+            return cmd;
+        }
+        /// <summary>
+        /// Defines the test method BatchCommandTest.
+        /// </summary>
+        /// <exception cref="System.Exception"></exception>
+        [Test]
+        public void BatchCommandTest()
+        {
+            try
+            {
+                List<BatchCommandList> value = _ga.RunBatchCommands(GetCommands(), out _errOut);
+                if (_errOut.Length > 0) throw new Exception(_errOut);
+
+                int testNumber = 1;
+                foreach (BatchCommandList v in value)
+                {
+                    string passfailed = v.PassedFailed ? "PASSED" : "FAILED";
+                    TestContext.WriteLine($"{testNumber}.) {passfailed} - {v.TestName}");
+                    TestContext.WriteLine(v.ReturnedValue);
+                    testNumber++;
+                }
+                //Assert.IsTrue(_ga.AllTestsPassed(value));
+            }
+            catch (Exception e)
+            {
+                TestContext.WriteLine($"ERROR: {e.Message}");
+                Assert.Fail();
+            }
+
+        }
+        /// <summary>
+        /// Defines the test method GenerateResultsTest.
+        /// </summary>
+        /// <exception cref="System.Exception"></exception>
+        /// <exception cref="System.Exception"></exception>
+        [Test]
+        public void GenerateResultsTest()
+        {
+            try
+            {
+                List<BatchCommandList> value = _ga.RunBatchCommands(GetCommands(), out _errOut);
+                if (_errOut.Length > 0) throw new Exception(_errOut);
+                TestContext.WriteLine(_ga.GenerateResults(value, out _errOut));
+                if (_errOut.Length > 0) throw new Exception(_errOut);
+                //Assert.IsTrue(value.Count > 0);
+            }
+            catch (Exception e)
+            {
+                TestContext.WriteLine($"ERROR: {e.Message}");
+                Assert.Fail();
+            }
+
+        }
     }
 }
