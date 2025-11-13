d9c97c7461e76e1a78645703adc534b5010fba90
BurnSoft <bs.github@burnsoft.net>
2025-11-13 17:15:47 -0500

    #41 added sample app application

---
+++ SampleUITestApp/frmMain.Designer.cs
@@ -0,0 +1,166 @@
+﻿namespace SampleUITestApp
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabMain = new System.Windows.Forms.TabPage();
+            this.tabOther = new System.Windows.Forms.TabPage();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.mnuFile = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuSave = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuExit = new System.Windows.Forms.ToolStripMenuItem();
+            this.btnClickTest = new System.Windows.Forms.Button();
+            this.lblClickStatus = new System.Windows.Forms.Label();
+            this.tabControl1.SuspendLayout();
+            this.tabMain.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabMain);
+            this.tabControl1.Controls.Add(this.tabOther);
+            this.tabControl1.Location = new System.Drawing.Point(0, 27);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(788, 423);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabMain
+            // 
+            this.tabMain.AccessibleName = "tabMain";
+            this.tabMain.Controls.Add(this.lblClickStatus);
+            this.tabMain.Controls.Add(this.btnClickTest);
+            this.tabMain.Location = new System.Drawing.Point(4, 22);
+            this.tabMain.Name = "tabMain";
+            this.tabMain.Padding = new System.Windows.Forms.Padding(3);
+            this.tabMain.Size = new System.Drawing.Size(780, 397);
+            this.tabMain.TabIndex = 0;
+            this.tabMain.Text = "Main";
+            this.tabMain.UseVisualStyleBackColor = true;
+            // 
+            // tabOther
+            // 
+            this.tabOther.AccessibleName = "tabOther";
+            this.tabOther.Location = new System.Drawing.Point(4, 22);
+            this.tabOther.Name = "tabOther";
+            this.tabOther.Padding = new System.Windows.Forms.Padding(3);
+            this.tabOther.Size = new System.Drawing.Size(780, 397);
+            this.tabOther.TabIndex = 1;
+            this.tabOther.Text = "Other";
+            this.tabOther.UseVisualStyleBackColor = true;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mnuFile});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // mnuFile
+            // 
+            this.mnuFile.AccessibleName = "mnuFile";
+            this.mnuFile.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mnuSave,
+            this.mnuExit});
+            this.mnuFile.Name = "mnuFile";
+            this.mnuFile.Size = new System.Drawing.Size(37, 20);
+            this.mnuFile.Text = "&File";
+            // 
+            // mnuSave
+            // 
+            this.mnuSave.AccessibleName = "mnuSave";
+            this.mnuSave.Name = "mnuSave";
+            this.mnuSave.Size = new System.Drawing.Size(180, 22);
+            this.mnuSave.Text = "&Save";
+            // 
+            // mnuExit
+            // 
+            this.mnuExit.AccessibleName = "mnuExit";
+            this.mnuExit.Name = "mnuExit";
+            this.mnuExit.Size = new System.Drawing.Size(180, 22);
+            this.mnuExit.Text = "&Exit";
+            // 
+            // btnClickTest
+            // 
+            this.btnClickTest.AccessibleName = "btnClickTest";
+            this.btnClickTest.Location = new System.Drawing.Point(22, 20);
+            this.btnClickTest.Name = "btnClickTest";
+            this.btnClickTest.Size = new System.Drawing.Size(75, 23);
+            this.btnClickTest.TabIndex = 0;
+            this.btnClickTest.Text = "Click Test";
+            this.btnClickTest.UseVisualStyleBackColor = true;
+            // 
+            // lblClickStatus
+            // 
+            this.lblClickStatus.AccessibleName = "lblClickStatus";
+            this.lblClickStatus.AutoSize = true;
+            this.lblClickStatus.Location = new System.Drawing.Point(22, 50);
+            this.lblClickStatus.Name = "lblClickStatus";
+            this.lblClickStatus.Size = new System.Drawing.Size(59, 13);
+            this.lblClickStatus.TabIndex = 1;
+            this.lblClickStatus.Text = "No Clicked";
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tabControl1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "frmMain";
+            this.Text = "Sample UI App For Testing";
+            this.tabControl1.ResumeLayout(false);
+            this.tabMain.ResumeLayout(false);
+            this.tabMain.PerformLayout();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabMain;
+        private System.Windows.Forms.TabPage tabOther;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem mnuFile;
+        private System.Windows.Forms.ToolStripMenuItem mnuSave;
+        private System.Windows.Forms.ToolStripMenuItem mnuExit;
+        private System.Windows.Forms.Label lblClickStatus;
+        private System.Windows.Forms.Button btnClickTest;
+    }
+}
+
