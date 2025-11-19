cc05c67ff9f3766279ab3e92e371beddfff9facb
BurnSoft <bs.github@burnsoft.net>
2025-11-19 16:35:26 -0500

    #40 Added the Universal Library to help wiht the process handling

---
+++ BurnSoft.Testing.Apps.Appium/AppiumHelper.cs
@@ -0,0 +1,90 @@
+﻿using OpenQA.Selenium.Appium.Service;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BurnSoft.Testing.Apps.Appium
+{
+    public class AppiumHelper
+    {
+        #region "Exception Error Handling"        
+        /// <summary>
+        /// The class location
+        /// </summary>
+        private static string _classLocation = "BurnSoft.Testing.Apps.Appium.AppiumHelper";
+
+        /// <summary>
+        /// Errors the message for regular Exceptions
+        /// </summary>
+        /// <param name="functionName">Name of the function.</param>
+        /// <param name="e">The e.</param>
+        /// <returns>System.String.</returns>
+        private static string ErrorMessage(string functionName, Exception e) => $"{_classLocation}.{functionName} - {e.Message}";
+        /// <summary>
+        /// Errors the message for access violations
+        /// </summary>
+        /// <param name="functionName">Name of the function.</param>
+        /// <param name="e">The e.</param>
+        /// <returns>System.String.</returns>
+        private static string ErrorMessage(string functionName, AccessViolationException e) => $"{_classLocation}.{functionName} - {e.Message}";
+        /// <summary>
+        /// Errors the message for invalid cast exception
+        /// </summary>
+        /// <param name="functionName">Name of the function.</param>
+        /// <param name="e">The e.</param>
+        /// <returns>System.String.</returns>
+        private static string ErrorMessage(string functionName, InvalidCastException e) => $"{_classLocation}.{functionName} - {e.Message}";
+        /// <summary>
+        /// Errors the message argument exception
+        /// </summary>
+        /// <param name="functionName">Name of the function.</param>
+        /// <param name="e">The e.</param>
+        /// <returns>System.String.</returns>
+        private static string ErrorMessage(string functionName, ArgumentException e) => $"{_classLocation}.{functionName} - {e.Message}";
+        /// <summary>
+        /// Errors the message for argument null exception.
+        /// </summary>
+        /// <param name="functionName">Name of the function.</param>
+        /// <param name="e">The e.</param>
+        /// <returns>System.String.</returns>
+        private static string ErrorMessage(string functionName, ArgumentNullException e) => $"{_classLocation}.{functionName} - {e.Message}";
+        #endregion
+        public event EventHandler<string> Errors;
+        public event EventHandler<string> JunkErrors;
+        protected virtual void SendError(string value)
+        {
+            Errors?.Invoke(this, value);
+        }
+        protected virtual void SendJunkError(string value)
+        {
+            JunkErrors?.Invoke(this, value);
+        }
+        private string _appiumApp;
+        private bool _buggerme;
+        public AppiumLocalService AppiumDriver;
+        public AppiumHelper(string appiumApp, bool debugMode) 
+        { 
+            _appiumApp = appiumApp;
+            _buggerme = debugMode;
+            AppiumDriver = AppiumLocalService.BuildDefaultService();
+        }
+        private bool StartAppium(string ip = "localhost", int port = 4723)
+        {
+            bool bAns = false;
+            try
+            {
+
+                bAns = true;
+            }
+            catch (Exception ex)
+            {
+                SendError(ErrorMessage("StartAppium", ex));
+            }
+            return bAns;
+        }
+
+    }
+}
