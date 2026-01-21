4012e670a8739e2fbdeb2f18140853e1c942910b
BurnSoft <bs.github@burnsoft.net>
2026-01-21 16:13:33 -0500

    #40 Added unit test for new helper class

---
+++ BurnSoft.Testing.Apps.Appium.NUnitTests/AppiumHelperTest.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+
+namespace BurnSoft.Testing.Apps.Appium.NUnitTests
+{
+    public class AppiumHelperTest
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
